2a63506c1c2847deb5567ad79683fe7b4167b20e
iamhyc <sudofree@163.com>
2021-06-21 13:03:04 +0800

    update plot and readme

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# GPS Analyzer for Raw Data
+
+### Prerequisite
+Install the runtime dependency
+
+```bash
+    pip3 install -r requirements
+```
+
+Install the video plot dependency
+```bash
+    sudo apt install ffmpeg
+```
+
+### Convert Raw GSP Data
+
+1. put raw data files with extension `*.log` in `./logs` folder ("");
+
+2. run `python3 ./log2gpx.py`, and the `*.gpx` files are generated correspondingly in `./gpx` folder.
+> Run `python3 ./log2gpx.py 2>/dev/null` to disable the WARNING message
+
+### Plot GSP Track
+```bash
+    python3 ./plotgpx.py ./gpx/<gpx_file>
+```
--- plotgpx.py
+++ plotgpx.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 from sys import argv
 from pathlib import Path
-from gpxplotter import read_gpx_file, create_folium_map, add_segment_to_map
-
-# Define some properties for drawing the line:
-line_options = {'color': 'red', 'weight': 8, 'opacity': 0.5}
 
 if len(argv) < 2:
     print('Usage: `plotgpx <gpx_file_path>`')
@@ -12,9 +8,23 @@ if len(argv) < 2:
 
 the_file = argv[1]
 the_name = Path(the_file).stem
-the_map = create_folium_map()
-for track in read_gpx_file(the_file):
-    for i, segment in enumerate(track['segments']):
-        add_segment_to_map(the_map, segment, line_options=line_options)
-    
-the_map.save( 'trajectory-%s.html'%the_name )
+
+## Using gpxplotter
+# from gpxplotter import read_gpx_file, create_folium_map, add_segment_to_map
+# the_map = create_folium_map()
+# # Define some properties for drawing the line:
+# line_options = {'color': 'red', 'weight': 8, 'opacity': 0.5}
+
+# for track in read_gpx_file(the_file):
+#     for i, segment in enumerate(track['segments']):
+#         add_segment_to_map(the_map, segment, line_options=line_options)
+# the_map.save( 'trajectory-%s.html'%the_name )
+
+
+## Using trackanimation
+import trackanimation
+from trackanimation.animation import AnimationTrack
+
+trk = trackanimation.read_track(the_file)
+fig = AnimationTrack(df_points=trk, dpi=300, bg_map=True, map_transparency=0.5)
+fig.make_video(output_file=the_name, framerate=60, linewidth=1.0)
