a896916c269f7ad6b96de4351e9652b4401d4ace
iamhyc <sudofree@163.com>
2021-06-30 21:26:33 +0800

    update test for sync_read

--- sync_read/src/main.rs
+++ sync_read/src/main.rs
@@ -7,16 +7,17 @@ use std::io::prelude::*;
 use rppal::gpio::Gpio;
 use serialport;
 
-const IO1:u8 = 23;
-const IO2:u8 = 12;
+const IO1:u8 = 27;
+const IO2:u8 = 23;
+const PACE:u64 = 1; //1 millisecond
 
 fn gps_recv(tx : mpsc::Sender<String>) {
     let timer = time::Instant::now();
     let mut serial_buf: Vec<u8> = vec![0; 1024];
 
-    let ports = serialport::available_ports().expect("No ports found!");
-    let port = ports[0].port_name.clone();
-    let mut port = serialport::new(port, 115_200)
+    // let ports = serialport::available_ports().expect("No ports found!");
+    // let port = ports[0].port_name.clone();
+    let mut port = serialport::new("/dev/ttyUSB0", 115_200)
             .timeout(time::Duration::from_millis(1000))
             .open().expect("Failed to open port");
     
@@ -41,10 +42,11 @@ fn file_writer(rx : mpsc::Receiver<String>) {
     loop {
         if let Ok(msg) = rx.recv_timeout(timeout) {
             file.write_all( msg.as_bytes() ).unwrap();
+            file.write_all( b"\n" ).unwrap();
             file.sync_data().unwrap();
         }
         else {
-            println!("[{:.6}] Timeout from GPS thread.", timer.elapsed().as_secs_f32());
+            println!("[{:.6}] Timeout from file-writer thread.", timer.elapsed().as_secs_f32());
         }
     }
 }
@@ -68,7 +70,7 @@ fn main() {
     });
 
     // main timer loop
-    let time_step = time::Duration::from_millis(1); //1ms
+    let time_step = time::Duration::from_millis(PACE);
     loop {
         thread::sleep(time_step);
         let io1 = pin1.read() as u8;
@@ -88,4 +90,37 @@ fn main() {
     //cleanup
     gps_handle.join().unwrap();
     writer_handle.join().unwrap();
+}
+
+#[test]
+fn gpio_read() {
+    let gpio = Gpio::new().unwrap();
+    let pin1 = gpio.get(IO1).unwrap().into_input();
+    let pin2 = gpio.get(IO2).unwrap().into_input();
+
+    let time_step = time::Duration::from_millis(1000);
+    loop {
+        thread::sleep(time_step);
+        let io1 = pin1.read() as u8;
+        let io2 = pin2.read() as u8;
+        println!("pin-{}: {}; pin-{}: {}", IO1, io1, IO2, io2);
+    }
+}
+
+#[test]
+fn gps_read() {
+    let mut serial_buf: Vec<u8> = vec![0; 1024];
+
+    let ports = serialport::available_ports().expect("No ports found!");
+    let port = ports[0].port_name.clone();
+    let mut port = serialport::new(port, 115_200)
+            .timeout(time::Duration::from_millis(1000))
+            .open().expect("Failed to open port");
+    
+    loop {
+        if let Ok(_) = port.read( serial_buf.as_mut_slice() ) {
+            let msg = String::from_utf8( serial_buf.clone() ).unwrap();
+            println!("{}", msg);
+        }
+    }
 }
